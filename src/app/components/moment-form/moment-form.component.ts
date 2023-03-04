67072bbade4b1e231bbf0f067d12d24345fa2585
João Gabriel <stuqzcrew@gmail.com>
2023-03-04 10:02:54 -0300

    Create form control and validation

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,12 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
   @Input() btnText!: String
 
+  momentForm!: FormGroup
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit(): void {
+    if(this.momentForm.invalid) {
+      return;
+    }
+
+  }
 
 }

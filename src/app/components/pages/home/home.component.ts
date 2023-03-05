53f68102b3fc16d99197a5b21b35bea4a1c61123
João Gabriel <stuqzcrew@gmail.com>
2023-03-05 18:38:12 -0300

    create home page and filter

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Moment } from 'src/app/Moment';
+import { MomentService  } from 'src/app/services/moment.service';
+
+import { environment  } from 'src/environments/invironment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [] // lista com filtro
+  baseApiUrl: string = environment.baseApiUrl
+
+  faSearch = faSearch;
+  searchTerm: string = '';
+
+  // todo search
+
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data
+
+      data.map(item => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR')
+      })
+
+      this.allMoments = data
+      this.moments = data
+    });
+
+  }
+
+  search(event: Event) {
+    const target = event.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter(moment => moment.title.toLowerCase().includes(value))
+  }
 
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { environment } from 'src/environments/invironment';
+
+import { Moment } from '../Moment';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +14,10 @@ export class MomentService {
   private baseApiUrl = environment.baseApiUrl
   private apiUrl = `${this.baseApiUrl}api/moments`
   constructor(private http: HttpClient) {
+  }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
   }
 
   createMoment(formData: FormData): Observable<FormData> {
